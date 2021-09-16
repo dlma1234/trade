bf9ad3ba0dbd44026c53e989d3e72396172f5c9f
Dana Ambos <danaambos@gmail.com>
2021-09-16 23:55:33 +0800

    Add authentication and tests

--- trade/api/tests.py
+++ trade/api/tests.py
@@ -1,3 +1,31 @@
-from django.test import TestCase
+import json
+
+from rest_framework.test import (
+    APITestCase, 
+    APIClient
+)
+from django.contrib.auth.models import User
+
+
+class TradeAPITest(APITestCase):
+
+    def setUp(self):
+        self.client = APIClient()
+
+    def test_user_logs_in_success(self):
+        user = User.objects.create_user(
+            username="test_user", email="test_user@test.com", password="testpass"
+        )
+
+        response = self.client.login(username="test_user", password="testpass")
+        self.assertTrue(response)
+
+    def test_user_logs_in_failed(self):
+        user = User.objects.create_user(
+            username="test_user", email="test_user@test.com", password="testpass"
+        )
+
+        response = self.client.login(username="test_user", password="testpass-xxxxx")
+        self.assertFalse(response)
 
 # Create your tests here.
--- trade/urls.py
+++ trade/urls.py
@@ -15,7 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from rest_framework.authtoken.views import obtain_auth_token
+
+from trade.api import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('authenticate/', obtain_auth_token, name='authentication'),
 ]
